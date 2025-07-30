ed97d416ac3b3a9197c2734463da74d5fde5e4e6
idangdev <idang.dev@gmail.com>
2025-07-30 11:01:22 +0700

    toast

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Home from "./pages/Home";
 import DetailPatients from "./features/patients/DetailPatients";
 import AddPatient from "./features/patients/AddPatient";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -29,6 +30,23 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: { duration: 3000 },
+          error: { duration: 5000 },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-70)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
